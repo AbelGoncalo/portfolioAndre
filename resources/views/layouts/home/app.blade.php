be93ca268209090b47be38ae60e10c52f0fa3f88
Abel Goncalo <abelgoncalo8@gmail.com>
2024-08-04 23:12:25 +0100

    Novo

--- resources/views/layouts/home/app.blade.php
+++ resources/views/layouts/home/app.blade.php
@@ -23,9 +23,9 @@
     <link
         href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:wght@400;500;700&display=swap"
         rel="stylesheet" />
-    <link rel="stylesheet" href="http://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <!-- Optional: Font Awesome for icons -->
-    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 </head>
 
