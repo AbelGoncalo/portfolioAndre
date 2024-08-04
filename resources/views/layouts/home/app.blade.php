65921da5ea0937e85d841b436363bd55f4ae5127
Abel Goncalo <abelgoncalo8@gmail.com>
2024-08-04 22:27:28 +0100

    Segundo commit

--- resources/views/layouts/home/app.blade.php
+++ resources/views/layouts/home/app.blade.php
@@ -15,12 +15,15 @@
     <link rel="stylesheet" href="{{ asset('home/vendors/lightbox/simpleLightbox.css') }}">
     <link rel="stylesheet" href="{{ asset('home/vendors/nice-select/css/nice-select.css') }}">
     <link rel="stylesheet" href="{{ asset('home/vendors/animate-css/animate.css') }}">
+
     <!-- main css -->
     <link rel="stylesheet" href="{{ asset('home/css/style.css') }}">
 
     <link
         href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=Poppins:wght@400;500;700&display=swap"
         rel="stylesheet" />
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -171,6 +174,7 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
     <x-livewire-alert::scripts />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
